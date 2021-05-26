9a0af452120be878840f629df530234ce0d4bded
zumrud7 <zumrudia@code.edu.az>
2021-05-26 04:28:22 +0400

    app and index are added

---
+++ e-commerce/src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
+import Query from "./components/Query";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Intro />
+      <ProductList />
+      <Query />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
